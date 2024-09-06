3b6a95179cf6da380f7adfdee77a9087d199b09a
wy <1059879308@qq.com>
2024-09-06 17:15:38 +0800

    test

--- classification_train.py
+++ classification_train.py
@@ -1,3 +1,6 @@
+import time
+
+import torch.optim
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 
@@ -5,6 +8,7 @@ from model import *
 from torch import nn
 from torch.utils.data import DataLoader
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Load the dataset
 
 train_dataset = torchvision.datasets.CIFAR10(root="data", train=True, transform=torchvision.transforms.ToTensor(), download=True)
@@ -19,25 +23,27 @@ print("测试长度为:{}".format(test_data_size))
 train_dataloader = DataLoader(train_dataset, batch_size=64)
 test_dataloader = DataLoader(test_dataset, batch_size=64)
 
-network = Network().cuda()
+network = Network().to(device)
 
-loss_function = nn.CrossEntropyLoss().cuda()
+loss_function = nn.CrossEntropyLoss().to(device)
 
 learning_rate = 1e-2
-optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(network.parameters(), lr=learning_rate)
 
 total_train_step = 0
 total_test_step = 0
 epoch = 10
 
 
-writer = SummaryWriter("/logs_train")
+writer = SummaryWriter("logs_train")
+start_time = time.time()
 for i in range(epoch):
     print("-----------第{}轮训练开始---------".format(i+1))
 
+    network.train()
     for data in train_dataloader:
         imgs, target = data
-        imgs, target = imgs.cuda(), target.cuda()
+        imgs, target = imgs.to(device), target.to(device)
         outputs = network(imgs)
         loss = loss_function(outputs, target)
 
@@ -47,18 +53,21 @@ for i in range(epoch):
 
         total_train_step += 1
         if total_train_step % 100 == 0:
-            print("训练次数:{}, 损失为:{}".format(total_train_step, loss.item()))
-            writer.add_scalar("train_loss", loss.item(), total_train_step)
+            end_time = time.time()
+            print("花费时间:{}".format(end_time - start_time))
+            print("训练次数:{}, 损失为:{}".format(total_train_step, loss))
+            writer.add_scalar("train_loss", loss, total_train_step)
 
+    network.eval()
     total_test_loss = 0.0
     total_accuracy = 0.0
     with torch.no_grad():
         for data in test_dataloader:
             imgs, target = data
-            imgs, target = imgs.cuda(), target.cuda()
+            imgs, target = imgs.to(device), target.to(device)
             outputs = network(imgs)
             loss = loss_function(outputs, target)
-            total_test_loss = total_test_loss + loss.item()
+            total_test_loss = total_test_loss + loss
             accuracy = (outputs.argmax(1) == target).sum()
             total_accuracy = total_accuracy + accuracy
 
---
+++ test.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+from PIL import Image
+
+image_path = "imgs/dog.png"
+image = Image.open(image_path)
+
+dict_sample = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat',4: 'deer',5: 'dog',6: 'frog',7: 'horse',
+               8: 'ship',9: 'truck'}
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Resize the image to 224x224
+image = image.resize((224, 224))
+
+transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)),
+                                            torchvision.transforms.ToTensor()])
+image = transform(image)
+print(image.shape)
+
+model = torch.load("network_9")
+print(model)
+image = torch.unsqueeze(image, dim=0).to(device)
+print(image.shape)
+
+# Set the model to evaluation mode and disable gradient computation
+model.eval()
+with torch.no_grad():
+    output = model(image)
+print(output)
+print(output.argmax(1).item())
+print(dict_sample.get(output.argmax(1).item()))
